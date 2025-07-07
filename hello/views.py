255d2ea71975234d0c88076cb1319befd72b57e9
v1m-m <19kalakija@chelsea-academy.org>
2025-07-07 09:03:51 +0000

    debug

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+import re
+from django.utils.timezone import datetime
 from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Hello, Django!")
 
-# Create your views here.
+def hello_there(request, name):
+    now = datetime.now()
+    formatted_now = now.strftime("%a, %d %b, %y at %X")
+
+    # Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only.
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return HttpResponse(content)
